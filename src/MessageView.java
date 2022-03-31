fc9c96f56218c8541ec6c2662c934f5efca9f853
nkiv <no.kivett@gmail.com>
2022-03-31 01:01:13 -0600

    first commit

---
+++ src/MessageView.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MessageView {
+    private final Scanner scanner = new Scanner(System.in); // stored here so we don't cause conflicts with System.in
+
+    public String messageToString(OpenMessage m){
+        String s = "TO: " + m.getTo() + "\n";
+        s += "FROM: " + m.getFrom() + "\n";
+        s += "SUBJECT: " + m.getSubject() + "\n";
+        s += "BODY: " + m.getBody() + "\n";
+        return s;
+    }
+    
+    public void printMenu(){
+
+    }
+
+    public String getInput(String msg){
+        msg = "";
+        String usrInput = scanner.nextLine();
+        return usrInput;
+    }
+
+}
